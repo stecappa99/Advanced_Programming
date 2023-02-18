d7aa37e6e95c9ab6c925682bcff3e9960606919d
stecappa99 <86434580+stecappa99@users.noreply.github.com>
2023-02-18 20:32:31 +0100

    Codice ottimizzato

--- StableMarriage/sm.py
+++ StableMarriage/sm.py
@@ -9,19 +9,24 @@ def sm(guy_pref, gal_pref):
         else:
             g = gs.pop(0)
             gal = guy_pref[g].pop(0)
-            i = gal_pref[gal].index(g)
             if gal not in couples:
                 print(f"Couple created {gal} & {g}")
-                couples[gal] = (i, g)
+                couples[gal] = g
             elif check_couple(gal_pref[gal], couples[gal], g):
-                print(f"Couple broke {gal} & {couples[gal][1]}")
-                gs.append(couples[gal][1])
+                print(f"Couple broke {gal} & {couples[gal]}")
+                gs.append(couples[gal])
                 print(f"Couple created {gal} & {g}")
-                couples[gal] = (i, g)
+                couples[gal] = g
             else:
                 gs.append(g)
             return find_couples(gs)
         
-    def check_couple(gal_ls, 
+    def check_couple(gal_ls, prec, current):
+        if gal_ls[0] == current:
+            return True
+        elif gal_ls[0] == prec:
+            return False
+        else:
+            return check_couple(gal_ls[1:], prec, current)
 
     return [(k, v[1]) for k, v in find_couples([k for k in guy_pref.keys()]).items()]
