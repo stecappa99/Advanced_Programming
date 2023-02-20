95e08789b771d9ac3f174b88d7d8c4a45c4da31a
stecappa99 <ste.cappa99@hotmail.it>
2023-02-20 22:09:13 +0100

    Aggiunti test sullo StableMarriage

--- StableMarriage/main.py
+++ StableMarriage/main.py
@@ -72,6 +72,32 @@ women4 = {
     "Ellen": ["Dani", "Bill", "Eric", "Carl", "Adam"]
 }
 
+guyprefers = {
+    'abe': ['abi', 'eve', 'isa', 'ivy', 'jan', 'dee', 'fay', 'bea', 'lin', 'may'],
+    'bob': ['isa', 'lin', 'abi', 'dee', 'eve', 'fay', 'bea', 'jan', 'ivy', 'may'],
+    'col': ['lin', 'eve', 'abi', 'dee', 'bea', 'fay', 'ivy', 'may', 'isa', 'jan'],
+    'dan': ['ivy', 'fay', 'dee', 'may', 'lin', 'eve', 'jan', 'bea', 'isa', 'abi'],
+    'gav': ['may', 'eve', 'ivy', 'bea', 'isa', 'abi', 'dee', 'lin', 'jan', 'fay'],
+    'hal': ['abi', 'eve', 'lin', 'fay', 'ivy', 'isa', 'jan', 'bea', 'may', 'dee'],
+    'ian': ['lin', 'isa', 'dee', 'may', 'bea', 'abi', 'fay', 'ivy', 'lin', 'may'],
+    'ike': ['jan', 'dee', 'bea', 'isa', 'fay', 'eve', 'abi', 'ivy', 'lin', 'may'],
+    'jon': ['abi', 'fay', 'jan', 'may', 'eve', 'bea', 'dee', 'isa', 'ivy', 'lin'],
+    'joy': ['bea', 'abi', 'dee', 'may', 'eve', 'ivy', 'isa', 'jan', 'lin', 'fay'],
+}
+
+galprefers = {
+    'abi': ['bob', 'joy', 'jon', 'gav', 'ian', 'abe', 'dan', 'ike', 'col', 'hal'],
+    'bea': ['bob', 'abe', 'col', 'joy', 'gav', 'dan', 'ian', 'ike', 'jon', 'hal'],
+    'isa': ['joy', 'bob', 'ike', 'gav', 'hal', 'col', 'ian', 'abe', 'dan', 'jon'],
+    'dee': ['joy', 'jon', 'col', 'abe', 'ian', 'hal', 'gav', 'dan', 'bob', 'ike'],
+    'eve': ['jon', 'hal', 'joy', 'dan', 'abe', 'gav', 'col', 'ike', 'ian', 'bob'],
+    'fay': ['bob', 'abe', 'ike', 'ian', 'jon', 'dan', 'joy', 'gav', 'col', 'hal'],
+    'may': ['jon', 'gav', 'hal', 'joy', 'bob', 'abe', 'col', 'ike', 'dan', 'ian'],
+    'lin': ['gav', 'jon', 'bob', 'abe', 'ian', 'dan', 'hal', 'ike', 'col', 'joy'],
+    'ivy': ['ian', 'col', 'hal', 'gav', 'joy', 'bob', 'abe', 'ike', 'jon', 'dan'],
+    'jan': ['ike', 'hal', 'gav', 'abe', 'bob', 'jon', 'col', 'ian', 'joy', 'dan'],
+}
+
 if __name__ == "__main__":
     print(sm(guy_pref, gal_pref))
     print()
@@ -82,3 +108,5 @@ if __name__ == "__main__":
     print(sm(men3, women3))
     print()
     print(sm(men4, women4))
+    print()
+    print(sm(guyprefers, galprefers))
