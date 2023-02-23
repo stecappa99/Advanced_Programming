bf061b9d810087038db1e31cb659928511beaabd
stecappa99 <86434580+stecappa99@users.noreply.github.com>
2023-02-23 10:06:28 +0100

    Ottimizzazione della ricerca e dell'accesso

--- StableMarriage/sm.py
+++ StableMarriage/sm.py
@@ -2,31 +2,23 @@
 
 def sm(guy_pref, gal_pref):
     couples = {}
-
+    sup = {k : {v[i]: i for i in range(len(v))} for k, v in gal_pref.items()}
     def find_couples(gs):
         if not len(gs):
             return couples
         else:
-            g = gs.pop(0)
+            g = gs.pop()
             gal = guy_pref[g].pop(0)
             if gal not in couples:
                 print(f"Couple created {gal} & {g}")
                 couples[gal] = g
-            elif check_couple(gal_pref[gal], couples[gal], g):
+            elif sup[gal][g] < sup[gal][couples[gal]]:
                 print(f"Couple broke {gal} & {couples[gal]}")
-                gs.append(couples[gal])
+                gs.insert(couples[gal], 0)
                 print(f"Couple created {gal} & {g}")
                 couples[gal] = g
             else:
-                gs.append(g)
+                gs.insert(g, 0)
             return find_couples(gs)
         
-    def check_couple(gal_ls, prec, current):
-        if gal_ls[0] == current:
-            return True
-        elif gal_ls[0] == prec:
-            return False
-        else:
-            return check_couple(gal_ls[1:], prec, current)
-
     return [(k, v) for k, v in find_couples([k for k in guy_pref.keys()]).items()]
